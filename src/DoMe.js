99a3be5c5b8bdb1b73da689a770b147a64b803f5
James Stephenson <2573758S@student.gla.ac.uk>
2022-06-21 16:56:41 +0100

    updated test for add button

---
+++ src/DoMe.js
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <>
+            <button aria-label={'add_button'}>
+                Add
+            </button>
+        </>
+            );
+}
--- src/DoMe.test.js
+++ src/DoMe.test.js
@@ -1,3 +1,9 @@
-describe('Do Me', () => {
+import {render, screen} from "@testing-library/react";
+import DoMe from "./DoMe";
 
+describe('Do Me', () => {
+    it('should have an add button', () => {
+        render(<DoMe />);
+        expect(screen.getByLabelText('add_button')).toBeInTheDocument();
+    })
 })
