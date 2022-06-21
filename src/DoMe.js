0ba712434337391121aed62364b7cb43985a4ed3
James Stephenson <2573758S@student.gla.ac.uk>
2022-06-21 14:29:40 +0100

    add a faulty html for displaying the todos

--- src/DoMe.js
+++ src/DoMe.js
@@ -7,10 +7,14 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import {Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function DoMe() {
     const [open, setOpen] = React.useState(false);
     const [currentInputText, setCurrentInputText] = React.useState('');
+    const [checked, setChecked] = React.useState([0]);
+
     const toDoArray = [];
     const ID = 0;
 
@@ -45,9 +49,51 @@ export default function DoMe() {
         handleClose();
     }
 
-    return (
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
 
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
+    return (
         <div>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {[0, 1, 2, 3].map((value) => {
+                    const labelId = `checkbox-list-label-${value}`;
+
+                    return (
+                        <ListItem
+                            key={value}
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="comments">
+                                    <EditIcon />
+                                </IconButton>
+                            }
+                            disablePadding
+                        >
+                            <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={checked.indexOf(value) !== -1}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{ 'aria-labelledby': labelId }}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
             <Button variant="outlined" onClick={handleClickOpen}>
                 <AcUnitIcon />
             </Button>
