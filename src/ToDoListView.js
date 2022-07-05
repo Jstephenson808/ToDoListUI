c64714addaa6ac9050991dfcb3eb8db510e45ca0
James Stephenson <jstephenson808@gmail.com>
2022-07-05 14:07:16 +0100

    add axios post functionality

--- src/ToDoListView.js
+++ src/ToDoListView.js
@@ -1,62 +1,77 @@
-import {Button, Dialog, DialogContent, DialogTitle, List, ListItem, TextField} from "@mui/material";
-import {useState} from "react";
+import { Button, Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 //TODO Convert tests to things which users see not internal labels
 
 // eslint-disable-next-line react/display-name
 export default function () {
-    const [addButtonOpenFlag, setAddButtonOpenFlag] = useState(false);
-    const [addItemTextBoxValue, setAddItemTextBoxValue] = useState('');
-    const [toDoList, setToDoList] = useState([]);
-    const [nextId, setNextId] = useState(0)
+  const [addButtonOpenFlag, setAddButtonOpenFlag] = useState(false);
+  const [addItemTextBoxValue, setAddItemTextBoxValue] = useState('');
+  const [toDoList, setToDoList] = useState([]);
 
-    const handleClose = () => {
-        setAddButtonOpenFlag(false);
-        setAddItemTextBoxValue('');
-    }
+  useEffect(() => {
+    axios.get('/todos').then((response) => {
+      setToDoList(response.data);
+    });
+  }, []);
 
-    const handleSave = () => {
-        setToDoList(toDoList.concat(createToDoObject(addItemTextBoxValue)));
-        handleClose();
-    }
+  const handleClickOpen = () => {
+    setAddButtonOpenFlag(true);
+  };
 
-    const createToDoObject = (textBoxValue) => {
-        const id = handleID();
-        const toDo = { id: id, value: textBoxValue};
-        return toDo;
-    }
+  const handleClose = () => {
+    setAddButtonOpenFlag(false);
+    setAddItemTextBoxValue('');
+  };
 
-    const handleID = () => {
-        const id = nextId;
-        setNextId(id+1);
-        return id;
-    }
+  const handleSave = () => {
+    axios
+      .post(`/todos`, {
+        addItemTextBoxValue,
+      })
+      .then((response) => {
+        const updatedToDos = [...toDoList, response.data];
+        setToDoList(updatedToDos);
+      });
+    handleClose();
+  };
 
+  // const createToDoObject = (textBoxValue) => {
+  //     const id = handleID();
+  //     const toDo = { id: id, value: textBoxValue};
+  //     return toDo;
+  // }
 
-    return (
-        <>
-            <List>
-                {toDoList.map((toDo) => (<ListItem key={toDo.id}> {(toDo.value)} </ListItem>))}
-            </List>
-            <Button aria-label={'add-button'} onClick={() => {
-                setAddButtonOpenFlag(true);
-            }}>
-                Add
-            </Button>
+  return (
+    <>
+      <List>
+        {toDoList.map((item) => (
+          <ListItem key={item.id}>
+            <ListItemText primary={item.name} />
+          </ListItem>
+        ))}
+      </List>
+      <Button aria-label={'add-button'} onClick={handleClickOpen}>
+        Add
+      </Button>
 
-            <Dialog open={addButtonOpenFlag} aria-label={'add-new-to-do'}>
-                <DialogTitle>Add New To Do</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        label="Enter To Do here"
-                        value={addItemTextBoxValue}
-                        onChange={(event) => setAddItemTextBoxValue(event.target.value)}
-                    />
-                </DialogContent>
-                <Button onClick={handleClose} aria-label={'cancel-add'}>Cancel</Button>
-                <Button onClick={handleSave} aria-label={'save-to-do'}>Save</Button>
-            </Dialog>
-
-        </>
-    );
+      <Dialog open={addButtonOpenFlag} aria-label={'add-new-to-do'}>
+        <DialogTitle>Add New To Do</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Enter To Do here"
+            value={addItemTextBoxValue}
+            onChange={(event) => setAddItemTextBoxValue(event.target.value)}
+          />
+        </DialogContent>
+        <Button onClick={handleClose} aria-label={'cancel-add'}>
+          Cancel
+        </Button>
+        <Button onClick={handleSave} aria-label={'save-to-do'}>
+          Save
+        </Button>
+      </Dialog>
+    </>
+  );
 }
